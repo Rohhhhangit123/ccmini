305bf2890f22dffe39824e2866bb7da71d160a02
Rohhhhangit123 <149319194+Rohhhhangit123@users.noreply.github.com>
2025-04-22 21:50:27 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_6aMnOBzQJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '8duvc1gie37uto4luovdt4e57', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_jynPzroYH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4gnkvkllnev2ini41vn905dsd8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
